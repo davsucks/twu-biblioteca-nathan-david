56ff57779f60a5817a33c4fea3f375a4daf0086e
dsucksto <davsucks@umich.edu>
2015-05-01 11:03:35 -0500

    Refactor main

--- src/com/twu/biblioteca/Main.java
+++ src/com/twu/biblioteca/Main.java
@@ -1,7 +1,9 @@
 package com.twu.biblioteca;
 
 import com.twu.biblioteca.Command.Command;
+import com.twu.biblioteca.Command.ListBooksCommand;
 
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -9,13 +11,20 @@ import java.util.Map;
 
 public class Main {
 
+    private static final PrintStream printStream = System.out;
+
     public static void main(String[] args) {
         Biblioteca biblioteca = new Biblioteca(books());
-        Map<String, Command> commandMap = new HashMap<>();
-        Menu menu = new Menu(System.out, biblioteca, new UserInput(), commandMap);
+        Menu menu = initializeMenu(biblioteca);
         Application application = new Application(menu);
 
         application.run();
+    }
+
+    private static Menu initializeMenu(Biblioteca biblioteca) {
+        Map<String, Command> commandMap = new HashMap<>();
+        commandMap.put("List Books", new ListBooksCommand(biblioteca, printStream));
+        return new Menu(printStream, biblioteca, new UserInput(), commandMap);
     }
 
     private static Collection<Book> books() {
